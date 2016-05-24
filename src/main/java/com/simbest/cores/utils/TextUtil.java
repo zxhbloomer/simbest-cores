acca4a8e377d4eaf5f0204c5b6588b00c0b97a40
kyne0116 <kyne.li@hotmail.com>
2016-05-24 23:44:54 +0800

    增加判断电话号码和手机号

--- src/main/java/com/simbest/cores/utils/TextUtil.java
+++ src/main/java/com/simbest/cores/utils/TextUtil.java
@@ -41,18 +41,36 @@ public class TextUtil {
         Matcher ma = pa.matcher(value);
         return ma.matches();
     }
+
+    public static boolean isMobile(String str) {
+        Pattern p = null;
+        Matcher m = null;
+        boolean b = false;
+        p = Pattern.compile("^[1][3,4,5,8][0-9]{9}$"); // 验证手机号
+        m = p.matcher(str);
+        b = m.matches();
+        return b;
+    }
+
     /**
      * 是否为电话
-     * @param value String
+     * @param str String
      * @return boolean
      */
-    public static boolean isPhone(String value) {
-        if (null == value || value.trim().length() == 0) {
-            return false;
+    public static boolean isPhone(String str) {
+        Pattern p1 = null,p2 = null;
+        Matcher m = null;
+        boolean b = false;
+        p1 = Pattern.compile("^[0][1-9]{2,3}-[0-9]{5,10}$");  // 验证带区号的
+        p2 = Pattern.compile("^[1-9]{1}[0-9]{5,8}$");         // 验证没有区号的
+        if(str.length() >9)
+        {   m = p1.matcher(str);
+            b = m.matches();
+        }else{
+            m = p2.matcher(str);
+            b = m.matches();
         }
-        Pattern pa = Pattern.compile("^(([(0-9)]+)|([0-9-]+))(([0-9-]+)|([0-9]+))([0-9])$");
-        Matcher ma = pa.matcher(value);
-        return ma.matches();
+        return b;
     }
 
 
